1acd87b7ff18dd6897b528e34579e4c290c4862c
DELL <orielnaim2001@gmail.com>
2022-09-08 10:45:46 +0300

    done to do unit test to Player.py object

--- game_card/Player.py
+++ game_card/Player.py
@@ -8,15 +8,22 @@ class Player:
 
     # a method that create player
     def __init__(self, _name_player, _num_per_player=26):
+        # check if type of name or type of num_per_player is correct
+        if type(_name_player) != str or type(_num_per_player) != int:
+            raise TypeError("The type of argument is wrong!!!")
+        # check if num_per_player is between 10-26, else put 26
         if _num_per_player < 10 or _num_per_player > 26:
             _num_per_player = 26
 
         self.name = _name_player
-        self.cards_player = []
+        self.cards_player = []  # empty list cards of player
         self.num = _num_per_player
 
     # method that create a deck for player
     def set_hand(self, deck: DeckOfCards):
+        # check the type if is DeckOfCards
+        if type(deck) != DeckOfCards:
+            raise TypeError("The deck must be type of DeckOfCards!!!")
         for card in range(self.num):
             self.cards_player.append(deck.deal_one())
 
@@ -28,6 +35,13 @@ class Player:
 
     # method that add card to player deck
     def add_card(self, card: Card):
+        # check if type of card is correct
+        if type(card) != Card:
+            raise TypeError("You can add only a card!!!")
+        # check if you add same card
+        for card_player in self.cards_player:
+            if card_player == card:
+                raise ValueError("The card exist in the list of cards!!!")
         self.cards_player.append(card)
 
     # return information about the player
---
+++ game_card/test_Player.py
@@ -0,0 +1,118 @@
+# Unit Test to Player Object
+from unittest import TestCase, mock
+from Player import Player
+from Card import Card
+from DeckOfCards import DeckOfCards
+
+
+class TestPlayer(TestCase):
+    def setUp(self):
+        self.player_dan_15 = Player("Dan", 15)
+        self.deck = DeckOfCards()  # deck of cards for tests
+
+    # check the valid arguments. the num argument need to be between 10-26
+    def test_init_valid(self):
+        player = Player("name_#@.", 10)
+        self.assertEqual(player.name, "name_#@.")
+        self.assertEqual(player.num, 10)
+        self.assertEqual(player.cards_player, [])
+        player = Player("name_#@. ", 25)
+        self.assertEqual(player.name, "name_#@. ")
+        self.assertEqual(player.num, 25)
+        self.assertEqual(player.cards_player, [])
+        player = Player("_#$%Name")
+        self.assertEqual(player.name, "_#$%Name")
+        self.assertEqual(player.num, 26)
+        self.assertEqual(player.cards_player, [])
+
+    # check invalid value num
+    def test_init_invalid_value(self):
+        player = Player("Name", 0)
+        self.assertEqual(player.num, 26)
+        self.assertEqual(player.cards_player, [])
+        player = Player("Name", -1)
+        self.assertEqual(player.num, 26)
+        self.assertEqual(player.cards_player, [])
+        player = Player("Name", 27)
+        self.assertEqual(player.num, 26)
+        self.assertEqual(player.cards_player, [])
+
+    # check the type of arguments
+    def test_init_invalid_type(self):
+        # check num
+        with self.assertRaises(TypeError):
+            player = Player("Name", 10.1)
+        with self.assertRaises(TypeError):
+            player = Player("Name", "25")
+        # check name
+        with self.assertRaises(TypeError):
+            player = Player(["Name"], 0)
+        with self.assertRaises(TypeError):
+            player = Player(123, 0)
+
+    # check if the cards_player in the correct length,
+    # and the card from deal_one in the list
+    @mock.patch('DeckOfCards.DeckOfCards.deal_one', return_value=Card(1, 4))
+    def test_set_hand_valid(self, mock_deal_one):
+        self.player_dan_15.set_hand(self.deck)
+        self.assertEqual(15, len(self.player_dan_15.cards_player))
+        self.assertIn(Card(1, 4), self.player_dan_15.cards_player)
+        # check default num_per_player
+        player = Player("name")
+        player.set_hand(self.deck)
+        self.assertEqual(26, len(player.cards_player))
+        self.assertIn(Card(1, 4), player.cards_player)
+
+    # check invalid deck
+    def test_set_hand_invalid(self):
+        with self.assertRaises(TypeError):
+            self.player_dan_15.set_hand([])
+        with self.assertRaises(TypeError):
+            self.player_dan_15.set_hand(12)
+
+    # check the function get_card work
+    def test_get_card_valid(self):
+        self.player_dan_15.cards_player.append(Card(1, 4))
+        self.player_dan_15.cards_player.append(Card(2, 4))
+        self.assertTrue(len(self.player_dan_15.cards_player) == 2)
+        self.assertEqual(Card(1, 4), self.player_dan_15.get_card())
+        self.assertTrue(len(self.player_dan_15.cards_player) == 1)
+
+    # check if the length of cards_player is 0
+    def test_get_card_invalid_length(self):
+        with self.assertRaises(IndexError):
+            self.player_dan_15.get_card()
+
+    # check the function add_card work
+    def test_add_card_valid(self):
+        self.player_dan_15.add_card(Card(1, 4))
+        self.assertTrue(len(self.player_dan_15.cards_player) == 1)
+        self.assertIn(Card(1, 4), self.player_dan_15.cards_player)
+
+    # check the type of deck
+    def test_add_card_invalid_type(self):
+        with self.assertRaises(TypeError):
+            self.player_dan_15.add_card(1)
+        with self.assertRaises(TypeError):
+            self.player_dan_15.add_card("Card")
+
+    # check if the method get same card in the list
+    def test_add_card_same_card(self):
+        self.player_dan_15.add_card(Card(1, 4))
+        with self.assertRaises(ValueError):
+            self.player_dan_15.add_card(Card(1, 4))
+
+    # check if the method add card to the list with length 52
+    def test_add_card_above_52_cards(self):
+        self.assertTrue(len(self.deck.deck) == 52)
+        # fill in hand of player 52 cards
+        for card in self.deck.deck:
+            self.player_dan_15.add_card(card)
+        self.assertTrue(len(self.player_dan_15.cards_player) == 52)
+        # add card (not exist in the list) to cards of player
+        with self.assertRaises(ValueError):
+            self.player_dan_15.add_card(Card(1, 5))
+        # add card (exist in the list) to cards of player
+        with self.assertRaises(ValueError):
+            self.player_dan_15.add_card(Card(1, 4))
+
